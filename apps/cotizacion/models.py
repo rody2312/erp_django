d8a48d6caef0f72b5127a955e517678aec953ba1
rody2312 <84883025+rody2312@users.noreply.github.com>
2023-06-20 19:18:45 -0400

    crud base de cotizacion agregado

--- apps/cotizacion/models.py
+++ apps/cotizacion/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from apps.oportunidad.models import Oportunidad
+
+class Cotizacion(models.Model):
+    id_cotizacion = models.AutoField(primary_key=True)
+    id_oportunidad = models.ForeignKey(Oportunidad, on_delete=models.CASCADE)
+    id_incoterm = models.IntegerField()
+    tipo_operacion = models.CharField(max_length=200)
+    fecha_cotizacion = models.DateField()
+    moneda = models.CharField(max_length=50)
+    tipo_cambio = models.CharField(max_length=50)
+    plazo_entrega = models.DateField()
+
+    class Meta:
+        db_table = 'cotizacion'
---
+++ apps/cotizacion/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import CotizacionListView, CotizacionDetailView, CotizacionCreateView, CotizacionUpdateView, CotizacionDeleteView
+
+urlpatterns = [
+    path('', CotizacionListView.as_view(), name='cotizacion_list'),
+    path('<int:pk>', CotizacionDetailView.as_view(), name='cotizacion_detail'),
+    path('new', CotizacionCreateView.as_view(), name='cotizacion_new'),
+    path('<int:pk>/edit', CotizacionUpdateView.as_view(), name='cotizacion_edit'),
+    path('<int:pk>/delete', CotizacionDeleteView.as_view(), name='cotizacion_delete'),
+]
